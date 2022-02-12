a812221be15171bf4777ffaa286ee9b6d188c0cc
ali-pixel38 <ali@pixel38.com>
2022-02-13 01:06:59 +0200

    done 156

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,10 +1,20 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+async function handler(req, res) {
     if (req.method === 'POST') {
         const userEmail = req.body.email;
         if (!userEmail || !userEmail.includes('@')) {
             res.status(422).json({ message: 'Invalid email address.' })
             return;
         }
+        const adminPassword = encodeURIComponent("12[]aszsa")
+        let url = `mongodb+srv://root:${adminPassword}@cluster0.svlvw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+        const client = await MongoClient.connect(url)
+        const db = client.db();
+        await db.collection('emails').insertOne({ email: userEmail });
+
+        client.close()
+
         console.log(userEmail)
         res.status(201).json({ message: 'Signed up!' })
     }
