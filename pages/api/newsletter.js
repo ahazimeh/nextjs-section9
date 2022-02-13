c8049008d5f6c8b02d85a0d2ddfd07224922a959
ali-pixel38 <ali@pixel38.com>
2022-02-13 12:21:46 +0200

    160

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,16 @@
 import { MongoClient } from "mongodb";
+async function connectDatabase() {
+    const adminPassword = encodeURIComponent(process.env.password)
+    let url = `mongodb+srv://root:${adminPassword}@cluster0.svlvw.mongodb.net/events?retryWrites=true&w=majority`;
+
+    const client = await MongoClient.connect(url)
+    return client;
+}
+
+async function insertDocument(client, document) {
+    const db = client.db();
+    await db.collection('newsletter').insertOne(document);
+}
 async function handler(req, res) {
     if (req.method === 'POST') {
         const userEmail = req.body.email;
@@ -9,11 +21,23 @@ async function handler(req, res) {
         const adminPassword = encodeURIComponent(process.env.password)
         let url = `mongodb+srv://root:${adminPassword}@cluster0.svlvw.mongodb.net/events?retryWrites=true&w=majority`;
 
-        const client = await MongoClient.connect(url)
-        const db = client.db();
-        await db.collection('newsletter').insertOne({ email: userEmail });
+        let client;
+        try {
+            client = await connectDatabase()
 
-        client.close()
+        }
+        catch (err) {
+            res.status(500).json({ message: 'Connecting to the database failed!' })
+            return
+        }
+        try {
+            await insertDocument(client, { email: userEmail })
+            client.close()
+        }
+        catch (err) {
+            res.status(500).json({ message: 'Inserting data failed!' })
+            return;
+        }
 
         console.log(userEmail)
         res.status(201).json({ message: 'Signed up!' })
